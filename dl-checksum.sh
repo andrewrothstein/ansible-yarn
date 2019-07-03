02d04144c9839d266433f39288ce40f6e7a5fc8f
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-07-03 13:42:41 -0500

    up to v1.17.2

---
+++ dl-checksum.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+VER=1.17.2
+DIR=~/Downloads
+MIRROR=https://github.com/yarnpkg/yarn/releases/download/v$VER
+FILE=yarn-v$VER.tar.gz
+URL=$MIRROR/$FILE
+LFILE=$DIR/$FILE
+
+if [ ! -e $LFILE ];
+then
+    wget -q -O $LFILE $URL
+fi
+
+printf "  # %s\n" $URL
+printf "  '%s': sha256:%s\n" $VER `sha256sum $LFILE | awk '{print $1}'`
+
+
