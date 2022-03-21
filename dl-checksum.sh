04514a5c38f2e4262e6a21953ecfdf9ccb7f332c
Andrew Rothstein <andrew.rothstein@gmail.com>
2022-03-20 20:53:21 -0400

    yarn:1.22.18

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -12,11 +12,11 @@ dl_ver()
 
     if [ ! -e $lfile ];
     then
-        wget -q -O $lfile $url
+        curl -sSLf -o $lfile $url
     fi
 
     printf "  # %s\n" $url
-    printf "  '%s': sha256:%s\n" $ver `sha256sum $lfile | awk '{print $1}'`
+    printf "  '%s': sha256:%s\n" $ver $(sha256sum $lfile | awk '{print $1}')
 }
 
-dl_ver ${1:-1.22.17}
+dl_ver ${1:-1.22.18}
